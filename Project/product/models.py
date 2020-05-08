da774428d35a6b6ad5b7df45fbd8f12050f25669
BuildTools <unconfigured@null.spigotmc.org>
2020-05-08 14:02:26 +0000

    Search-bar implementation

--- Project/product/models.py
+++ Project/product/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+
 # Create your models here.
 class Product(models.Model):
     # TODO: Add the rest of the attributes required
--- Project/product/views.py
+++ Project/product/views.py
@@ -5,6 +5,7 @@ from product.models import Product
 
 # INSERT INTO product_product (name,color,price,"imgURL",description,discount)
 
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -12,6 +13,25 @@ from product.models import Product
 def index(request):
     context = {'products': Product.objects.all()}
     return render(request, 'product/index.html', context)
+
+
+def index(request):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        products = [ {
+            'id': x.id,
+            'name': x.name,
+            'color': x.color,
+            'price': x.price,
+            'imgURL': x.imgURL,
+            'description': x.description,
+            'discount': x.discount
+        } for x in Product.objects.filter(name__icontains=search_filter)]
+        return JsonResponse({'data': products})
+
+    context = {'products': Product.objects.all()}
+    return render(request, 'product/index.html', context)
+
 
 # SUPPORTS -> /product/1
 def getProductById(request, id):
