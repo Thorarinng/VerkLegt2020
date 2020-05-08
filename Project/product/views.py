2cfef4b448b1367134508efc9683fb7df69096b5
fridriks98 <42949259+fridriks98@users.noreply.github.com>
2020-05-08 14:24:28 +0000

    merged with Kjartan

--- Project/product/views.py
+++ Project/product/views.py
@@ -10,6 +10,19 @@ from django.http import JsonResponse
 # Create your views here.
 # This gets rendered when http://127.0.0.1:8000/products is run, which is also the default
 def index(request):
+    if 'search_filter' in request.GET:
+        search_filter = request.GET['search_filter']
+        products = [ {
+            'id': x.id,
+            'name': x.name,
+            'color': x.color,
+            'price': x.price,
+            'imgURL': x.imgURL,
+            'description': x.description,
+            'discount': x.discount
+        } for x in Product.objects.filter(name__icontains=search_filter)]
+        return JsonResponse({'data': products})
+
     if 'price' in request.GET:
         price_filter = request.GET['price']
         if price_filter == 'low':
@@ -41,25 +54,6 @@ def index(request):
             return JsonResponse({'data': products})
     context = {'products': Product.objects.all()}
     return render(request, 'product/index.html', context)
-
-
-def index(request):
-    if 'search_filter' in request.GET:
-        search_filter = request.GET['search_filter']
-        products = [ {
-            'id': x.id,
-            'name': x.name,
-            'color': x.color,
-            'price': x.price,
-            'imgURL': x.imgURL,
-            'description': x.description,
-            'discount': x.discount
-        } for x in Product.objects.filter(name__icontains=search_filter)]
-        return JsonResponse({'data': products})
-
-    context = {'products': Product.objects.all()}
-    return render(request, 'product/index.html', context)
-
 
 # SUPPORTS -> /product/1
 def getProductById(request, id):
