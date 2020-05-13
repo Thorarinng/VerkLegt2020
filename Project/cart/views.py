17f70df45e5abdcb26f9078c2b68e428811a3918
Thorarinn S Gunnarsson <torarinn91@gmail.com>
2020-05-13 10:38:08 +0000

    Added message when removing item form cart

--- Project/cart/views.py
+++ Project/cart/views.py
@@ -8,19 +8,16 @@ from django.contrib import messages
 # Create your views here.
 
 
-def addToCart(request, id):
+def addToCart(request, productId):
     product = Product.objects.get(id=id)
-    # request.session.setdefault('cart', {})[str(id)] = product.productToDict()
     # Guard against default cookies without cart.
+    # If cart hasn't been created in the cookies before, we do it here
     try:
         if request.session['cart'] is None:
             request.session['cart'] = {}
     except:
         request.session['cart'] = {}
-    request.session['cart'][str(id)] = product.productToDict(id)
-    # request.session['cart'].append(product.productToDict())
-    # return render(request, 'base.html', request.session['cart'])
-    # return render(request, 'product/index.html')
+    request.session['cart'][str(productId)] = product.productToDict(productId)
     request.session.modified = True
     messages.success(request, 'Added to cart')
     return redirect("/")
@@ -31,9 +28,14 @@ def getCart(request):
     return render(request, 'cart/cart.html', context)
 
 
-def removeFromCart(request, id):
-    print(request.session['cart'][str(id)])
-    del request.session['cart'][str(id)]
+def removeFromCart(request, productId):
+    print(request.session['cart'][str(productId)])
+    # Removing the certain product from the cart
+    del request.session['cart'][str(productId)]
+    # Committing the changes
     request.session.modified = True
+    # dictionary ret value to return
     context = {'cart': request.session['cart']}
+    # Message displayed when item removed
+    messages.warning(request, 'Item removed from cart')
     return render(request, 'cart/cart.html', context)
